c7f28830dfd9104af3bc2f2a2d7dc4821c37ccb1
Diego Malone <diegomalone@gmail.com>
2021-06-01 17:44:48 -0300

    Slide up screen when keyboard is shown to allow editing the bottom textField

--- Udacity MemeMe/ViewController.swift
+++ Udacity MemeMe/ViewController.swift
@@ -22,12 +22,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     ]
     
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        subscribeToKeyboardNotifications()
     }
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         configureTextField(textField: topTextField, defaultText: "TOP TEXT")
         configureTextField(textField: bottomTextField, defaultText: "BOTTOM TEXT")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
     }
     
     private func configureTextField(textField: UITextField, defaultText: String) {
@@ -75,6 +83,33 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if textField.text!.isEmpty {
             textField.text = "YOUR TEXT HERE"
         }
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    func unsubscribeFromKeyboardNotifications() {
+
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+
+        view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+
+        view.frame.origin.y = 0
+    }
+
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
     }
 }
 
