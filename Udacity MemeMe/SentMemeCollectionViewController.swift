9209a14bd944f55bc31e0210ca72ad54e89c399d
Diego Malone <diegomalone@gmail.com>
2021-06-06 08:10:44 -0300

    Add data logic to the sent meme collection tab

--- Udacity MemeMe/SentMemeCollectionViewController.swift
+++ Udacity MemeMe/SentMemeCollectionViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class SentMemeCollectionViewController: UICollectionViewController {
     
+    var memes: [Meme]! {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.memes
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let item: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(SentMemeTableViewController.createMeme))
         self.navigationItem.rightBarButtonItem = item
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sentMemeCollectionCell", for: indexPath) as! SentMemeCollectionCellView
+        let meme = memes[(indexPath as NSIndexPath).row]
+        
+        cell.memeImage.image = meme.memeImage
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return memes.count
     }
     
     @objc func createMeme() {
---
+++ Udacity MemeMe/UI Elements/SentMemeCollectionCellView.swift
@@ -0,0 +1,14 @@
+//
+//  SentMemeCollectionCellView.swift
+//  Udacity MemeMe
+//
+//  Created by Diego Malone on 06/06/21.
+//
+
+import Foundation
+import UIKit
+
+class SentMemeCollectionCellView: UICollectionViewCell {
+    
+    @IBOutlet weak var memeImage: UIImageView!
+}
