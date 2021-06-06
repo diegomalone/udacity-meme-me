253a20baf38120c5705f9a37d18b62ea71581c76
Diego Malone <diegomalone@gmail.com>
2021-06-05 23:55:53 -0300

    Create sent meme screen structure

---
+++ Udacity MemeMe/SentMemeCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SentMemeCollectionViewController.swift
+//  Udacity MemeMe
+//
+//  Created by Diego Malone on 05/06/21.
+//
+
+import Foundation
+import UIKit
+
+class SentMemeCollectionViewController: UICollectionViewController {
+    
+}
---
+++ Udacity MemeMe/SentMemeTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SentMemeTableViewController.swift
+//  Udacity MemeMe
+//
+//  Created by Diego Malone on 05/06/21.
+//
+
+import Foundation
+import UIKit
+
+class SentMemeTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell() // TODO
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
