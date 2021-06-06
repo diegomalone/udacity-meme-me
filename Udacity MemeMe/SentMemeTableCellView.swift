57f6115a07c535e30c45b818dbf54d7ce2d10155
Diego Malone <diegomalone@gmail.com>
2021-06-06 01:23:55 -0300

    Add custom cell view to the meme table view

---
+++ Udacity MemeMe/SentMemeTableCellView.swift
@@ -0,0 +1,15 @@
+//
+//  SentMemeTableCellView.swift
+//  Udacity MemeMe
+//
+//  Created by Diego Malone on 06/06/21.
+//
+
+import Foundation
+import UIKit
+
+class SentMemeTableCellView: UITableViewCell {
+    
+    @IBOutlet weak var memeImage: UIImageView!
+    @IBOutlet weak var memeText: UILabel!
+}
--- Udacity MemeMe/SentMemeTableViewController.swift
+++ Udacity MemeMe/SentMemeTableViewController.swift
@@ -10,19 +10,34 @@ import UIKit
 
 class SentMemeTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    var memes: [Meme]! {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.memes
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
         
         let item: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(SentMemeTableViewController.createMeme))
         self.navigationItem.rightBarButtonItem = item
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell() // TODO
+        let cell = tableView.dequeueReusableCell(withIdentifier: "sentMemeCell", for: indexPath) as! SentMemeTableCellView
+        let meme = memes[(indexPath as NSIndexPath).row]
+        
+        cell.memeImage.image = meme.memeImage
+        cell.memeText.text = "\(meme.topText) \(meme.bottomText)"
+        
+        return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return memes.count
     }
     
     @objc func createMeme() {
